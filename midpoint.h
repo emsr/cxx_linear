68ffbc1fa7f7ce96608fce7023f2dc01893297f4
Edward M Smith-Rowland <esmith-rowland@alionscience.com>
2018-04-11 15:11:24 -0400

    Update linear->lerp.

--- midpoint.h
+++ midpoint.h
@@ -5,18 +5,20 @@ namespace std
 {
 
   template<typename _Float>
-    linear(_Float __a, _Float __b, _Float __t)
+    std::enable_if_t<_Float, std::is_floating_point_v<_Float>>
+    lerp(_Float __a, _Float __b, _Float __t)
     {
       if (std::isnan(__a) || std::isnan(__b) || std::isnan(__t))
 	return std::numeric_limits<_Float>::quiet_NaN();
-      else if (__a * __b <= 0)
+      else if (__a <= 0 && __b >= 0 || __a >= 0 && __b <= 0)
+	// ab <= 0 but product could overflow.
 	return __t * __b + (1 - __t) * __a;
-      else if (__t == 1)
+      else if (__t == _Float{1})
 	return __b;
       else
-	{
+	{ // monotonic near t == 1.
 	  const auto __x = __a + __t * (__b - __a);
-	  return __t > 1 == __b > __a
+	  return __t > _Float{1} == __b > __a
 		 ? std::max(__b, __x)
 		 : std::min(__b,__x);
 	}
